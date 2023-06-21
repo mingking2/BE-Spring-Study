6e82f6db7112d20d94e83ef1259f4a9973ee2764
King's meow <mingee641@gmail.com>
2023-06-21 12:14:12 +0900

    modifiedAt 오류 해결 : Timestamped.java에 @Getter 추가

--- src/main/java/com/sparta/springBasic/week03/domain/Timestamped.java
+++ src/main/java/com/sparta/springBasic/week03/domain/Timestamped.java
@@ -2,6 +2,7 @@ package com.sparta.springBasic.week03.domain;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass // Entity가 자동으로 컬럼으로 인식한다.
 @EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트한다.
+@Getter
 public class Timestamped {
 
     @CreatedDate
