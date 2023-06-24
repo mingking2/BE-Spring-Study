e8489a92ce3781c3bb87c5224c42e2960bb14827
King's meow <mingee641@gmail.com>
2023-06-24 23:54:52 +0900

    [4주차] 자바로 네이버 쇼핑 API 사용하기

---
+++ src/main/java/com/sparta/springBasic/week04/utils/NaverShopSearch.java
@@ -0,0 +1,29 @@
+package com.sparta.springBasic.week04.utils;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class NaverShopSearch {
+    public String search() {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-id", "0It0gGjlxGtuDHhC2PBv");
+        headers.add("X-Naver-Client-Secret", "YG5fv1NecA");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=adidas&display=30&sort=asc", HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = (HttpStatus) responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public static void main(String[] args) {
+        NaverShopSearch naverShopSearch = new NaverShopSearch();
+        naverShopSearch.search();
+    }
+}
